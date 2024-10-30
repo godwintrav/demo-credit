a6ff17bdd3ff2d9a116b676722f9b55f39c3a9f8
Godwin Odenigbo <godwintrav@gmail>
2024-10-30 04:54:30 +0000

    configure jest for testing

---
+++ src/__test__/app.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "../app";
+
+describe("GET /", () => {
+  it("should return a welcome message", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Welcome to the demo credit API!");
+  });
+});
