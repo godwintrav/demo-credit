b02df84a06938a93d5c1005d3a7a5e5c3b5d7b2a
Godwin Odenigbo <godwintrav@gmail>
2024-10-30 07:06:25 +0000

    database configured

---
+++ src/migrations/20241030061341_create_user_transaction_account_table.ts
@@ -0,0 +1,49 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("email").notNullable().unique();
+    table.string("bvn").notNullable().unique();
+    table.string("phone_number").notNullable().unique();
+    table.date("date_of_birth").notNullable();
+    table.integer("lga_id").notNullable();
+    table.string("city").notNullable();
+    table.string("address").notNullable();
+    table.string("password").notNullable();
+    table.timestamps(true, true);  // created_at, updated_at
+  }).createTable("transactions", (table) => {
+    table.increments("id").primary();
+    table
+    .integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("users")
+    .onDelete("CASCADE"); // Deletes all user's transactions if user is deleted
+    table
+    .enum("transaction_type", ["fund", "withdraw", "transfer"], {
+      useNative: true,
+      enumName: "transaction_type_enum",
+    })
+    .notNullable();
+    table.decimal("amount", 10, 2).notNullable();
+    table.string("reference").nullable();
+    table.timestamps(true, true);  // created_at, updated_at
+  }).createTable("accounts", (table) => {
+    table.increments("id").primary();
+    table
+    .integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("users")
+    .onDelete("CASCADE"); // Deletes user account if user is deleted
+    table.decimal("balance", 10, 2).defaultTo(0.0).notNullable();
+    table.timestamps(true, true);  // created_at, updated_at
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("accounts").dropTableIfExists("transactions").dropTableIfExists("users");
+}
