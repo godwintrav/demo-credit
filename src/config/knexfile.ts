9209786e66580eca3e5fc137a19360e55df23721
Godwin Odenigbo <godwintrav@gmail>
2024-10-30 07:05:43 +0000

    database configured

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -1,19 +1,27 @@
 import type { Knex } from "knex";
 import dotenv from "dotenv";
-
+import fs from "fs";
 dotenv.config();
+
 const DB_CLIENT = "mysql2";
+const MIGRATION_PATH = "../migrations";
+
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: DB_CLIENT,
     connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
-      database: process.env.DB_NAME || "database",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+      ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(process.env.PATH_TO_SSL_CERTIFICATE as string).toString(),
+      },
     },
     migrations: {
-      directory: "./src/migrations",
+      directory: MIGRATION_PATH,
     },
   },
   production: {
@@ -23,9 +31,14 @@ const config: { [key: string]: Knex.Config } = {
       user: process.env.PROD_DB_USER,
       password: process.env.PROD_DB_PASSWORD,
       database: process.env.PROD_DB_NAME,
+      port: Number(process.env.PROD_DB_PORT),
+      ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(process.env.PATH_TO_SSL_CERTIFICATE as string).toString(),
+      }
     },
     migrations: {
-      directory: "./src/migrations",
+      directory: MIGRATION_PATH,
     },
   },
 };
